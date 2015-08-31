08f8c387ab3e6a589b8651682c64b4c620a5bbf6
cout970 <thecout970@gmail.com>
2015-08-31 02:51:43 +0200

    fixed progres bar bug
    Signed-off-by: cout970 <thecout970@gmail.com>

--- BewomInstaller/src/es/bewom/downloads/DownloadProgress.java
+++ BewomInstaller/src/es/bewom/downloads/DownloadProgress.java
@@ -1,41 +1,48 @@
 package es.bewom.downloads;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
 
 import es.bewom.BewomPack;
 
 public class DownloadProgress extends Thread {
-	
+
 	public double p;
 	public String dirFile;
-	
-	public void set(double p, String dirFile){
-		
+
+	public void set(double p, String dirFile) {
 		this.p = p;
 		this.dirFile = dirFile;
-		
 	}
-	
+
 	@Override
 	public void run() {
-		
+
 		File fios = new File(dirFile);
-		
-		while (true){
-			
-			int progress = (int) ((100/p) * fios.length());
-			
-			BewomPack.progressBar.setValue(progress);
-			
-			if(progress == 100){
-				
-				this.interrupt();
+		while (true) {
+			int progress = (int) Math.round(((100 / p) * fios.length()));
+
+			if (progress >= 100) {
 				break;
-				
 			}
-			
+
+			Runnable run = new Runnable() {
+
+				@Override
+				public void run() {
+					BewomPack.progressBar.setValue(progress);
+				};
+			};
+			try {
+				SwingUtilities.invokeAndWait(run);
+			} catch (InvocationTargetException | InterruptedException e) {
+				e.printStackTrace();
+			}
+
 		}
-		
+		this.interrupt();
 	}
 
 }
